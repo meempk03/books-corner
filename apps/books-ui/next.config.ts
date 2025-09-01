ab336918377292ad49e3a0594470e5fe499b1366
Meenakshi KP <meenakshikp03@gmail.com>
2025-09-01 12:09:47 +0200

    Set up nx monorepo

---
+++ apps/books-ui/next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from "next";
+import { composePlugins, withNx } from "@nx/next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  nx: {
+    svgr: false,
+  }
+};
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+const plugins = [
+  // Add more Next.js plugins to this list if needed.
+  withNx,
+];
+
+// export default nextConfig;
+export default composePlugins(...plugins)(nextConfig);
