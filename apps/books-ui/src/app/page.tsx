5fe569314ad686e970afe1b703d4619180da2083
Meenakshi KP <meenakshikp03@gmail.com>
2026-01-24 12:51:13 +0100

    Comment the code

--- apps/books-ui/src/app/page.tsx
+++ apps/books-ui/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default async function Home() {
-  const books = await fetch(`http://localhost:4000/books?search='harrypotter'`).then((res) => res.json());
-  console.log('boooks', books);
+  // const books = await fetch(`http://localhost:4000/books?search='harrypotter'`).then((res) => res.json());
+  // console.log('boooks', books);
   return (
     <>
       {/* Hero Section */}
