c2b56b344085646dafb4705185df08a42407e089
Meenakshi KP <meenakshikp03@gmail.com>
2026-01-31 19:06:05 +0100

    fix the dist folder issue

--- apps/books-api/src/index.ts
+++ apps/books-api/src/index.ts
@@ -8,8 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-// export default app;
-
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
 });
