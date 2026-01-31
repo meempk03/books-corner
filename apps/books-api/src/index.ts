792cddd725ecf4fc6306ae8eee84fe36f389a79e
Meenakshi KP <meenakshikp03@gmail.com>
2026-01-31 13:30:29 +0100

    Enable express app deployment on vercel

---
+++ apps/books-api/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { router } from './routers/books';
+
+// const host = process.env.HOST ?? 'localhost';
+// const port = process.env.PORT ? Number(process.env.PORT) : 4000;
+
+const app = express();
+app.use(express.json());
+app.use(router);
+
+export default app;
+
+// app.listen(port, host, () => {
+//   console.log(`[ ready ] http://${host}:${port}`);
+// });
