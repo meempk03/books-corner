09d1e09d9259d7c05c7690b3ac8b4a83f5b7cf74
Meenakshi KP <meenakshikp03@gmail.com>
2026-06-28 16:33:04 +0200

    Migrate to next js 16

--- apps/books-ui/next.config.ts
+++ apps/books-ui/next.config.ts
@@ -1,15 +1,21 @@
 import type { NextConfig } from "next";
 import { composePlugins, withNx } from "@nx/next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
   nx: {
     svgr: false,
   },
   images: {
-    domains: ['covers.openlibrary.org'],
+    // domains: ['covers.openlibrary.org'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'covers.openlibrary.org',
+      },
+    ],
   },
-};
+} as NextConfig;
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
