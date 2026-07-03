3a78572cd2398becca2185b95fc0ab02202b568f
Meenakshi KP <meenakshikp03@gmail.com>
2026-07-03 16:49:27 +0200

    Fix build warnings

--- apps/books-ui/next.config.ts
+++ apps/books-ui/next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
-import { composePlugins, withNx } from "@nx/next";
 
-const nextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig: import('next').NextConfig = {
   /* config options here */
-  nx: {
-    svgr: false,
-  },
   images: {
     // domains: ['covers.openlibrary.org'],
     remotePatterns: [
@@ -17,13 +14,5 @@ const nextConfig = {
   },
 } as NextConfig;
 
-/**
- * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
- **/
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
-
 // export default nextConfig;
-export default composePlugins(...plugins)(nextConfig);
+export default nextConfig;
